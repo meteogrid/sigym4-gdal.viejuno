7ef6e788917cc0a8c82c4f292c8bdcc704d0a85c
Alberto Valverde <alberto@toscat.net>
2015-01-28 09:23:43 +0100

    Upgraded to latest sigym4-geometry and bumped up version

--- Sigym4/GDAL.hs
+++ Sigym4/GDAL.hs
@@ -1,21 +1,26 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Sigym4.GDAL (
     createGeoreferencedDataset
   , module GDAL
   , module OSR
 ) where
 
+import Data.Proxy (Proxy(..))
 import Sigym4.Geometry
 import OSGeo.GDAL as GDAL
 import OSGeo.OSR as OSR
 
 
+createGeoreferencedDataset ::
+     forall srid t. (KnownNat srid, HasDataset Dataset ReadWrite t)
+  => GeoReference V2 srid -> String -> DriverOptions -> Int -> FilePath
+  -> IO (Dataset ReadWrite t)
 createGeoreferencedDataset geoRef dName dOpts nBands fPath = do
   let Size (V2 nx ny) = grSize geoRef
       gt              = toGDALGeoTrans $ grTransform geoRef
-      srs             = case grSrs geoRef of
-                          SrsProj4 proj4 -> fromProj4 proj4
-                          SrsEPSG epsg   -> fromEPSG epsg
+      srs             = fromEPSG . fromIntegral $ gSrid (Proxy :: Proxy srid)
   ds <- create dName fPath nx ny nBands dOpts
   setDatasetGeotransform ds gt
   case srs of
@@ -23,7 +28,7 @@ createGeoreferencedDataset geoRef dName dOpts nBands fPath = do
     Right srs' -> setDatasetProjection ds (toWkt srs')
   return ds
 
-toGDALGeoTrans :: GeoTransform V2 -> GDAL.Geotransform
+toGDALGeoTrans :: GeoTransform V2 srid -> GDAL.Geotransform
 toGDALGeoTrans gt = Geotransform a b c d e f
   where
     V2 (V2 b e) (V2 c f) = gtMatrix gt
